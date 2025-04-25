c224dc819b5f73ffa62eb47a360a2cccf7c11d2e
tranglh13 <tranglh13@ghtk.co>
2025-04-25 14:30:28 +0700

    tc

--- Scripts/TC_1/TC_7/Script1745551149896.groovy
+++ Scripts/TC_1/TC_7/Script1745551149896.groovy
@@ -45,3 +45,6 @@ WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/inp
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
 
 WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.acceptAlert()
+
